4eb2d9fe39829fb021838b325c4e26b92198725f
Yabiee-5 <e205744@ie.u-ryukyu.ac.jp>
2022-10-28 00:30:43 +0900

    ToDoコンポーネント作った

--- react-todo-app/src/App.tsx
+++ react-todo-app/src/App.tsx
@@ -3,12 +3,11 @@ import './App.css';
 import TodoList from './components/TodoList';
 
 function App() {
-  const [todos, setTodos] = useState(['Todo1'])
+  const [todos, setTodos] = useState(['Todo1','Todo2'])
 
   return (
     <div className="App">
       <TodoList todos={todos}/>
-      <input></input>
       <button>タスクを追加</button>
       <button>完了したタスク</button>
     </div>
